fbfa255bf1c98e88be8a29005cb2588d102d5800
Kelvin <kelvin@convertr.com.br>
2025-01-30 22:14:40 -0300

    criado seeds e populamos o banco

---
+++ app/Models/Beach.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Beach extends Model
+{
+
+    protected $table = 'beaches';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'beach_name', 'latitude', 'longitude'
+    ];
+
+}
---
+++ database/seeders/BeachSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+
+
+class BeachSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('beaches')->insert([
+            'beach_name' => 'Navegantes',
+            'latitude' => '-26.8989',
+            'longitude' => '-48.6542'
+        ]);
+    }
+}
