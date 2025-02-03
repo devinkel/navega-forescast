a60f4e28e792fb960b78e6d84cf1422c58995775
kelvin <kelvin@convertr.com.br>
2025-02-03 12:05:49 -0300

    added getAll Service/ForecastServices

--- app/Services/Internal/ForecastService.php
+++ app/Services/Internal/ForecastService.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Services\Internal;
 
 use App\Models\Forecasts;
@@ -6,10 +7,11 @@ use GuzzleHttp\Psr7\Query;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Validator;
 
-class ForecastService {
+class ForecastService
+{
 
-    public function create(array $forecastData) {
-
+    public function create(array $forecastData)
+    {
         $validator = Validator::make($forecastData, [
             'beach_id' => 'required|exists:beaches,id',
             'forecast' => 'required|array',
@@ -19,31 +21,36 @@ class ForecastService {
             return response()->json([
                 'error' => true,
                 'message' => $validator->errors()
-            ],400);
+            ], 400);
         }
 
         try {
             $forecast = Forecasts::create($forecastData);
-
             return response()->json([
                 'error' => false,
                 'message' => 'Previsão criada com sucesso',
                 'data' => $forecast
             ], 200);
-
         } catch (QueryException $e) {
-
             return response()->json([
                 'error' => true,
                 'message' => $e->getMessage()
-            ],500);
-        
+            ], 500);
         } catch (\Exception $e) {
-            
             return response()->json([
                 'error' => true,
                 'message' => $e->getMessage()
-            ],500);
+            ], 500);
         }
+    }
+
+    public function getAll()
+    {
+        $forecasts = Forecasts::all();
+
+        return response()->json([
+            'error' => false,
+            'data' => $forecasts
+        ], 200);
     }
 }
--- tests/Services/Internal/BeachSeviceTest.php
+++ tests/Services/Internal/BeachSeviceTest.php
@@ -11,7 +11,7 @@ class ForecastSeviceTest extends TestCase
     use DatabaseTransactions;
     /**
      * @testdox --ForecastService : Falha ao criar forecast com beach_id inválido
-     *
+     *        
      * @group Forecast
      */
 
@@ -20,7 +20,7 @@ class ForecastSeviceTest extends TestCase
         $invalidForecast =  [
             'beach_id' => 999, // ID não existe
             'forecast' => [
-                '2025-02-01' => [
+                '2025-02-01' =>[ 
                     '00:00' => [
                         'swell_wave_height' => '0.48m',
                         'wave_height' => '0.7m',
@@ -39,7 +39,7 @@ class ForecastSeviceTest extends TestCase
 
     /**
      * @testdox --ForecastService : Falha ao criar forecast sem beach_id
-     *
+     *        
      * @group Forecast
      */
 
@@ -47,7 +47,7 @@ class ForecastSeviceTest extends TestCase
     {
         $invalidForecast =  [
             'forecast' => [
-                '2025-02-01' => [
+                '2025-02-01' =>[ 
                     '00:00' => [
                         'swell_wave_height' => '0.48m',
                         'wave_height' => '0.7m',
@@ -64,53 +64,37 @@ class ForecastSeviceTest extends TestCase
         $this->assertEquals(json_decode($response->getContent(), true)['message']['beach_id'], ['The beach id field is required.']);
     }
 
-    /**
+        /**
      * @testdox --ForecastService : Sucesso ao criar um foracast
-     *
+     *        
      * @group Forecast
      */
 
-    public function testForecastSuccess()
-    {
-        $validForecast =  [
-            'beach_id' => 1, // Supondo que esse ID não exista
-            'forecast' => [
-                '2025-02-03' => [
-                    '00:00' => [
-                        'swell_wave_height' => '0.48m',
-                        'wave_height' => '0.7m',
-                    ],
-                ]
-            ]
-        ];
+     public function testForecastSuccess()
+     {
+         $validForecast =  [
+             'beach_id' => 1, // Supondo que esse ID não exista
+             'forecast' => [
+                 '2025-02-03' =>[ 
+                     '00:00' => [
+                         'swell_wave_height' => '0.48m',
+                         'wave_height' => '0.7m',
+                     ],
+                 ]
+             ]
+         ];
+ 
+         $newForecast = new ForecastService();
+ 
+         $response = $newForecast->create($validForecast);
+         $validResponseToTest = json_decode(json_encode($response->getData()->data), true);
 
-        $newForecast = new ForecastService();
+         unset($validResponseToTest['updated_at']);
+         unset($validResponseToTest['created_at']);
+         unset($validResponseToTest['id']);
 
-        $response = $newForecast->create($validForecast);
-        $validResponseToTest = json_decode(json_encode($response->getData()->data), true);
-
-        unset($validResponseToTest['updated_at']);
-        unset($validResponseToTest['created_at']);
-        unset($validResponseToTest['id']);
-
-        $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals(json_decode($response->getContent(), true)['error'], false);
-        $this->assertEquals($validResponseToTest, $validForecast);
-    }
-
-    /**
-     * @testdox --ForecastService : Retorna todos os forecasts
-     *
-     * @group Forecast
-     */
-
-    public function testForecastReturnAllForecasts()
-    {
-        $newForecast = new ForecastService();
-
-        $forecasts = $newForecast->getAll();
-        $formatedForescasts = json_decode($forecasts->getContent(), true);
-        $this->assertEquals($formatedForescasts['error'], false);
-        $this->assertNotEmpty($formatedForescasts['data']);
-    }
+         $this->assertEquals($response->getStatusCode(), 200);
+         $this->assertEquals(json_decode($response->getContent(), true)['error'], false);
+         $this->assertEquals($validResponseToTest, $validForecast);
+     }
 }
